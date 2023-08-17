1fde0cb5e0576d28dbef043a399af96b2bedb11c
John Daugherty <Johndaugherty1986@gmail.com>
2023-08-17 14:13:20 -0500

    adding state, moving esbuil

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
   return (
     <div className="App">
-      <textarea title="codebox"></textarea>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        title="codebox"
+      ></textarea>
       <div>
-        <button>submit</button>
-        <pre></pre>
+        <button onClick={onClick}>submit</button>
+        <pre>{code}</pre>
       </div>
     </div>
   );
